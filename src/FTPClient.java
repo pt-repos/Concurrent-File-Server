1121feacf538a32dc280abacbda87fe75c1c6d95
Pulkit T <p.tripathi@ufl.edu>
2019-10-08 15:32:01 -0400

    Implemented dir (retrieve list of available files) and upload (upload file to server) commands.

--- src/FTPClient.java
+++ src/FTPClient.java
@@ -1,12 +1,14 @@
-import java.io.IOException;
+import java.io.*;
 import java.net.ConnectException;
 import java.net.Socket;
 
 public class FTPClient {
 
-    Socket requestSocket;
-    String host;
-    int port;
+    private Socket requestSocket;
+    private String host;
+    private int port;
+    private ObjectInputStream inputStream;
+    private ObjectOutputStream outputStream;
 
     public FTPClient(String host, int port) {
         this.host = host;
@@ -14,28 +16,111 @@ public class FTPClient {
     }
 
     public static void main(String[] args) {
-        String host = args[0];
-        int port = Integer.parseInt(args[1]);
-        FTPClient client = new FTPClient(host, port);
+//        String host = args[0];
+//        int port = Integer.parseInt(args[1]);
+//        FTPClient client = new FTPClient(host, port);
+        FTPClient client = new FTPClient("localhost", 8000);
         client.run();
     }
+
+    private void sendMessage(String message) throws IOException {
+        outputStream.writeObject(message);
+        outputStream.flush();
+    }
+
+//    private void sendMessage(String... message) throws IOException {
+//        StringBuilder stringBuilder = new StringBuilder();
+//        for (String msg: message) {
+//            stringBuilder.append(msg + " ");
+//        }
+//        outputStream.writeObject(stringBuilder.toString());
+//        outputStream.flush();
+//    }
+
+    private void uploadFile(String fileName) throws IOException {
+        FileInputStream fileInputStream = null;
+        BufferedInputStream bufferedInputStream = null;
+        DataInputStream dataInputStream = null;
+        try {
+            File file = new File(".//files//client1//" + fileName);
+            byte[] buffer = new byte[(int) file.length()];
+            fileInputStream = new FileInputStream(file);
+            bufferedInputStream = new BufferedInputStream(fileInputStream);
+            dataInputStream = new DataInputStream(bufferedInputStream);
+            dataInputStream.readFully(buffer, 0, buffer.length);
+
+            outputStream.writeUTF(fileName);
+            outputStream.writeLong(buffer.length);
+            outputStream.write(buffer, 0, buffer.length);
+            outputStream.flush();
+        } catch (FileNotFoundException e) {
+            System.err.println("Cannot find file");
+        } finally {
+            if (null != fileInputStream) fileInputStream.close();
+            if (null != bufferedInputStream) bufferedInputStream.close();
+            if (null != dataInputStream) dataInputStream.close();
+        }
+    }
+
+//    private void receiveFile(String responseStatus) {
+//        file
+//    }
 
     void run() {
         try {
             requestSocket = new Socket(host, port);
-            System.out.println("Connected to " + host + "on port " + port);
-        } catch(ConnectException e) {
+            System.out.println("Connected to " + host + " on port " + port);
+            outputStream = new ObjectOutputStream(requestSocket.getOutputStream());
+            outputStream.flush();
+            inputStream = new ObjectInputStream(requestSocket.getInputStream());
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+            while (true) {
+                System.out.println("Input Command");
+                String[] command = bufferedReader.readLine().split(" ");
+                switch (command[0]) {
+                    case "dir":
+                        sendMessage(command[0]);
+                        String files = (String) inputStream.readObject();
+                        System.out.println("Available files: " + files);
+                        break;
+
+                    case "upload":
+                        if (command.length < 2) {
+                            System.out.println("No File name specified");
+                        }
+                        else {
+                            sendMessage(command[0]);
+                            uploadFile(command[1]);
+                        }
+
+                        break;
+
+//                    case "get":
+//                        if (command.length < 2) {
+//                            System.out.println("No file name specified");
+//                        }
+//                        else {
+//                            sendMessage(command[0]);
+//                            sendMessage(command[1]);
+//                            String responseStatus = (String) inputStream.readObject();
+//                            receiveFile(responseStatus);
+//                        }
+
+                    default:
+                        break;
+                }
+
+            }
+        } catch (ConnectException e) {
             System.out.println("Initiate Server");
-        } catch (IOException e) {
+        } catch (IOException | ClassNotFoundException e) {
             e.printStackTrace();
         } finally {
             try {
-                if (null != requestSocket) {
-                    requestSocket.close();
-                }
+                if (null != requestSocket) requestSocket.close();
+                if (null != outputStream) outputStream.close();
+                if (null != inputStream) inputStream.close();
             } catch (IOException e) {
-                e.printStackTrace();
-            } catch (Exception e) {
                 e.printStackTrace();
             }
         }
--- src/FTPServer.java
+++ src/FTPServer.java
@@ -1,18 +1,29 @@
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.HashSet;
+import java.util.Set;
 
 public class FTPServer {
-    
+
     private static final int sPort = 8000;
+    private static Set<String> fileSet;
+    
+    public FTPServer() {}
 
     public static void main(String[] args) throws Exception {
         System.out.println("Server is running");
         ServerSocket listener = new ServerSocket(sPort);
         int clientId = 0;
+
+        fileSet = new HashSet<>();
+        fileSet.add("File1");
+        fileSet.add("File2");
+
         try {
-            while(true) {
+            while (true) {
                 new Handler(listener.accept(), ++clientId).start();
-                System.out.println("Client " + clientId + "connected");
+                System.out.println("Client " + clientId + " connected");
             }
         }
         finally {
@@ -23,14 +34,75 @@ public class FTPServer {
     private static class Handler extends Thread {
         private Socket connection;
         private int clientId;
+        private ObjectInputStream inputStream;
+        private ObjectOutputStream outputStream;
+        private FileInputStream fileInputStream;
+        private FileOutputStream fileOutputStream;
 
         private Handler(Socket connection, int clientId) {
             this.connection = connection;
             this.clientId = clientId;
         }
 
-        public void run() {
+        private void sendAvailableFiles() throws IOException {
+            StringBuffer files = new StringBuffer();
+            for (String fileName: fileSet) {
+                files.append(fileName + ", ");
+            }
+            outputStream.writeObject(files.toString());
+            outputStream.flush();
+        }
 
+        private void receiveFile(String fileName) throws IOException {
+            fileOutputStream = new FileOutputStream(".//files//server//" + fileName);
+            Long fileSize = inputStream.readLong();
+            byte[] buffer = new byte[1024];
+
+            int bytesRead;
+            while (fileSize > 0
+                    && (bytesRead = inputStream.read(buffer, 0, (int) Math.min(buffer.length, fileSize))) != -1) {
+
+                fileOutputStream.write(buffer, 0, bytesRead);
+                fileSize -= bytesRead;
+            }
+            fileOutputStream.close();
+            fileSet.add(fileName);
+        }
+
+        public void run() {
+            try {
+                outputStream = new ObjectOutputStream(connection.getOutputStream());
+                outputStream.flush();
+                inputStream = new ObjectInputStream(connection.getInputStream());
+
+                while (true) {
+                    try {
+                        String command = (String) inputStream.readObject();
+                        System.out.println(command + " command received from client" + clientId);
+
+                        switch (command) {
+                            case "dir":
+                                sendAvailableFiles();
+                                break;
+
+                            case "upload":
+                                String fileName = inputStream.readUTF();
+                                receiveFile(fileName);
+                                break;
+
+//                            case "get":
+                        
+                            default:
+                                System.out.println("Unknown command. Please try again.");
+                                break;
+                        }
+					} catch (ClassNotFoundException e) {
+                        System.err.println("Data received in unknown format");
+					}
+                }
+			} catch (IOException e) {
+                System.out.println("Disconnected with Client " + clientId);
+            }
         }
     }
 
